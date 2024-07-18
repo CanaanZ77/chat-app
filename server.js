8c6730b4bcefa4f57b4f2398179c27d624ab356e
CanaanZ77 <canaanzhang@mailfence.com>
2024-07-17 18:45:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Listening on *:3000');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Listening on *:${port}`);
 });
